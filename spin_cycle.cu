97e96b2b113d3485f3416a9f8097ed69705db440
Duncan McFarlane <dmcf2@pdx.edu>
2024-07-02 09:44:18 -0700

    Added debug messages and updated spin kernel to further the use of inline ptx

--- spin_cycle.cu
+++ spin_cycle.cu
@@ -1,11 +1,13 @@
-#include <cuda_runtime.h>
 #include <cooperative_groups.h>
+#include <cuda_runtime.h>
 #include <stdio.h>
 #include <unistd.h>
 
 #define HISTOGRAM_SIZE 32  // clock values are 32 bit
 #define THREADS_PER_BLOCK 256
 #define BLOCKS_NUM 32
+#define REPS 100000000
+#define DEBUG
 
 __device__ unsigned int histogram[HISTOGRAM_SIZE];
 
@@ -16,23 +18,55 @@ __global__ void spin(unsigned long int reps) {
   for (int i = threadIdx.x; i < HISTOGRAM_SIZE; i += blockDim.x) {
     sharedHistogram[i] = 0;
   }
-  __syncthreads();
+
+  // Sync threads
+  asm volatile("bar.sync 0;");
+
+  unsigned int tid = threadIdx.x;
+  unsigned int bid = blockIdx.x;
+  unsigned int bdim = blockDim.x;
 
   for (unsigned int i = 0; i < reps; i++) {
-    __syncthreads();
-    unsigned int t1, t2;
-    asm volatile("mov.u32 %0, %%clock;" : "=r"(t1)::"memory");
-    asm volatile("mov.u32 %0, %%clock;" : "=r"(t2)::"memory");
+    unsigned int t1, t2, diff, bin;
 
-    //Log_2 binning
-    atomicAdd(&sharedHistogram[__clz(t2 - t1)], 1);
+    // Sync threads
+    asm volatile("bar.sync 0;");
+
+    // Query clock reg & calc difference
+    asm volatile(
+        "mov.u32 %0, %%clock;\n\t"
+        "mov.u32 %1, %%clock;\n\t"
+        "sub.u32 %2, %1, %0;\n\t"
+        : "=r"(t1), "=r"(t2), "=r"(diff)
+        :
+        : "memory");
+
+    // log_2 bining of diff using clz
+    asm volatile("clz.b32 %0, %1;\n\t" : "=r"(bin) : "r"(diff));
+
+#ifdef DEBUG
+    if (tid == 0 && i == 0) {
+      printf("t1: %u, t2: %u, diff: %u\n", t1, t2, diff);
+    }
+#endif
+
+    // Atomic add to shared histogram
+    asm volatile("atom.shared.add.u32 %0[%1], 1;\n\t"
+                 :
+                 : "l"(sharedHistogram), "r"(bin * 4)
+                 : "memory");
   }
 
-  __syncthreads();
+  // Sync threads
+  asm volatile("bar.sync 0;");
 
-  // move shared into global mem
-  for (int i = threadIdx.x; i < HISTOGRAM_SIZE; i += blockDim.x) {
-    atomicAdd(&histogram[i], sharedHistogram[i]);
+  // Move shared into global mem
+  for (int i = tid; i < HISTOGRAM_SIZE; i += bdim) {
+    unsigned int val = sharedHistogram[i];
+    asm volatile("atom.global.add.u32 %0[%1], %2;\n\t"
+                 :
+                 : "l"(histogram), "r"(i * 4), "r"(val)
+                 : "memory");
   }
 }
 
@@ -50,12 +84,15 @@ void runHistogram(unsigned long int reps) {
     cudaGetDeviceProperties(&prop, i);
 
     // Header
-    // Ripped from https://developer.nvidia.com/blog/how-query-device-properties-and-handle-errors-cuda-cc/
+    // Ripped from "Querying Device Properties" in
+    // https://developer.nvidia.com/blog/how-query-device-properties-and-handle-errors-cuda-cc/
     printf("Device Number: %d\n", i);
     printf("\tName: %s\n", prop.name);
     printf("\tMemory Clock Rate (KHz): %d\n", prop.memoryClockRate);
     printf("\tMemory Bus Width (bits): %d\n", prop.memoryBusWidth);
-    printf("\tPeak Memory Bandwidth (GB/s): %f\n\n", 2.0 * prop.memoryClockRate * (prop.memoryBusWidth / 8) / 1.0e6);
+    printf("\tPeak Memory Bandwidth (GB/s): %f\n\n",
+           2.0 * prop.memoryClockRate * (prop.memoryBusWidth / 8) / 1.0e6);
+    // END of copied code
     printf("\t# Blocks (hardcoded) %d\n", BLOCKS_NUM);
     printf("\t# Threads per block (hardcoded) %d\t", THREADS_PER_BLOCK);
     printf("\t# repetitions %d\t", reps);
@@ -69,7 +106,7 @@ void runHistogram(unsigned long int reps) {
     // Copy results back to host
     cudaMemcpy(h_histogram, histogram, HISTOGRAM_SIZE * sizeof(unsigned int),
                cudaMemcpyDeviceToHost);
-    
+
     // Print Results
     for (int i = 0; i < HISTOGRAM_SIZE; i++) {
       printf("Bin %d: %u\n", i, h_histogram[i]);
@@ -77,19 +114,8 @@ void runHistogram(unsigned long int reps) {
   }
 }
 
-
 int main() {
-  /*while ((opt = getopt(argc, argv, "n:")) != -1) {
-    switch (opt) {
-      case 'n':
-        reps = atoi(optarg);  
-        break;
-      default:
-        fprintf(stderr, "Usage: %s [-n integer number of repetitions]\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
-  }*/
-  unsigned long int reps = 100000;
+  unsigned long int reps = REPS;
   runHistogram(reps);
   return 1;
 }
